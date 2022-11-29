8eb6349de2497c63a5bb9ea4b8fb7e988bf4256d
Admin <Admin@DESKTOP-H6TCJOO>
2022-11-29 19:53:26 +0530

    add

---
+++ Activity2/src/com/question2/Arithemetic.java
@@ -0,0 +1,20 @@
+package com.question2;
+
+public class Arithemetic {
+	
+	
+		int add(int a,int b)
+		{
+			return a+b;
+		}
+}
+	
+	class Adder extends Arithemetic
+	{
+		
+	}
+
+
+
+		
+
---
+++ Activity2/src/com/question3/Customermain.java
@@ -0,0 +1,19 @@
+package com.question3;
+
+public class Customermain {
+	
+
+	public static void main(String[] args) {
+		
+		Customer c=new Customer();
+		
+		c.createAccount();
+		c.setAmount();
+		c.getAmount();
+		c.withDrawAmount();
+		
+		
+
+	}
+
+}
