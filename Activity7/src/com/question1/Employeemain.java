ef463c0a386f30266861388ae300a3ecccdd048c
Admin <Admin@DESKTOP-H6TCJOO>
2022-12-29 19:21:39 +0530

    Activity7

---
+++ Activity7/src/com/question1/Employeemain.java
@@ -0,0 +1,27 @@
+package com.question1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Employeemain {
+
+	public static void main(String[] args) {
+		Employee e1=new Employee(1,"Akshay",19,50000);
+		Employee e2=new Employee(3,"Atish",20,60000);
+		Employee e3=new Employee(5,"Mahesh",18,30000);
+		Employee e4=new Employee(4,"Vaibhav",19,20000);
+		
+		ArrayList<Employee> el=new ArrayList<>();
+		el.add(e1);				//adding the element to the list
+		el.add(e2);
+		el.add(e3);
+		el.add(e4);
+		
+		Collections.sort(el);		//sorting the element in ascending order
+		for(Employee o:el)
+		{
+			System.out.println(o.id+" "+o.name+" "+o.age+" "+o.salary);
+		}
+
+}
+}
